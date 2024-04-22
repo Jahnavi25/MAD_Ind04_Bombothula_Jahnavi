821307e8678eec583cc47da307b8981790e3a2a4
Jahnavi Bombothula <jahnavi.bombothula@okstate.edu>
2024-04-21 20:50:00 -0500

    Initial Commit

---
+++ MAD_Ind04_Bombothula_Jahnavi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MAD_Ind04_Bombothula_Jahnavi
+//
+//  Created by Jahnavi Bombothula on 4/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
