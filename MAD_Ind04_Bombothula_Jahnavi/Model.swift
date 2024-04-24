5014dd2508ce2f598a44192409d0a544327bcc96
Jahnavi Bombothula <jahnavi.bombothula@okstate.edu>
2024-04-23 22:40:28 -0500

    23rd april 10:40

---
+++ MAD_Ind04_Bombothula_Jahnavi/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  MAD_Ind04_Bombothula_Jahnavi
+//
+//  Created by Jahnavi Bombothula on 4/23/24.
+//
+
+import Foundation
---
+++ MAD_Ind04_Bombothula_Jahnavi/View.swift
@@ -0,0 +1,8 @@
+//
+//  view.swift
+//  MAD_Ind04_Bombothula_Jahnavi
+//
+//  Created by Jahnavi Bombothula on 4/23/24.
+//
+
+import Foundation
